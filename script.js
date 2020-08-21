9ce8d4e0a6a6ff38b82443ff067d89a6244370ba
FrancescaMahoney <FrancescaMahoney@users.noreply.github.com>
2020-08-21 12:41:36 +0100

    Updated HTML CSS & JS files with Weather App code

--- script.js
+++ script.js
@@ -1,1 +1,127 @@
-alert("Hello");
+function formatDate(date) {
+  let currentDate = date.getDate();
+  let year = date.getFullYear();
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  let monthIndex = date.getMonth();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[monthIndex];
+
+  return `${day} ${currentDate} ${month} ${year} | ${hours}:${minutes}`;
+}
+
+function displayWeatherConditions(response) {
+  document.querySelector("#city-display").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = response.data.wind.speed;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchLocation(position) {
+  let apiKey = "7078ca8e45a8e54ad9b485826d119586";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherConditions);
+}
+
+function searchCity(city) {
+  let apiKey = "7078ca8e45a8e54ad9b485826d119586";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherConditions);
+}
+
+function displayBackgroundImage(response) {
+  document.querySelection("#body-background");
+}
+
+function searchImageLocation(natural) {
+  let apiKey = "BHQKLBephGK3GnkK56FsQZ6TpKgMdTtrYCw0wUVq";
+  let apiUrl = `https://api.nasa.gov/EPIC/api/natural/date/2019-05-30?api_key=${apiKey}`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayBackgroundImage);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-text-input").value;
+  searchCity(city);
+  searchImageLocation(natural);
+}
+
+function getLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = temperatureElement.innerHTML;
+  temperature = Number(temperature);
+  temperatureElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = temperatureElement.innerHTML;
+  temperature = Number(temperature);
+  temperatureElement.innerHTML = Math.round(((temperature - 32) * 5) / 9);
+}
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+
+dateElement.innerHTML = formatDate(currentTime);
+
+let searchNewCity = document.querySelector("#search-city");
+searchNewCity.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
+
+searchCity("London");
+
+let currentLocationButton = document.querySelector("#location-button");
+currentLocationButton.addEventListener("click", getLocation);
+
+let bodyBackgroundImage = document.querySelector("#body-background");
